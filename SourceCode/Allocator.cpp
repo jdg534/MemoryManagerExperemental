1f68fb9bb1803737ebf948164d8a4280323acf4d
Josh <joshdgibson21@outlook.com>
2016-11-07 23:20:06 +0000

    Added more structure to the allocator

--- SourceCode/Allocator.cpp
+++ SourceCode/Allocator.cpp
@@ -7,10 +7,36 @@
 #include <iostream>
 #endif 
 
+
+namespace AllocatorAnonNameSpace
+{
+	Heap * f_instPtr = nullptr;
+}
+
+// rember Heap can't use new or delete
+
+Heap * Heap::getInstance()
+{
+
+}
+
+Heap::Heap()
+{
+
+}
+
+Heap::~Heap()
+{
+
+}
+
+
 void * operator new(std::size_t size)
 {
 	// size_t actualNumBytesToAllocate = size + sizeof(AllocationChunk);
 	// std::printf("%zu Bytes requested\n", size);
+
+
 	void * allocLoc = std::malloc(size);
 #ifdef _DEBUG
 	std::cout << size << " bytes requested at " << allocLoc << std::endl;
--- SourceCode/Allocator.h
+++ SourceCode/Allocator.h
@@ -3,15 +3,37 @@
 
 #include <cstdio>
 
+#define FREE_MEMORY 0xFFFFFFFF
+#define ALLOCATION_GUARD 0xDEADCODE
+
+// 5MB heap
+#define HEAP_SIZE 1024 * 1024 * 5
+
+
 struct AllocationChunk
 {
-	void * baseAddress;
+	unsigned int startingGuard;
 	size_t nBytesAllocated;
+	void * baseAddress;
 };
+
+class Heap
+{
+private:
+	static Heap * getInstance();
+	Heap()
+	{
+	}
+
+	~Heap()
+	{
+	}
+	char m_rawBytes[HEAP_SIZE];
+	void * m_startingAdress;
+};
+
 
 void * operator new(std::size_t size);
 void operator delete(void * addressToDelete);
-
-
 
 #endif // _ALLOCATOR_H_
